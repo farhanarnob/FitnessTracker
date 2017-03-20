d4d0139361fde3248ad084bf3200a93c6e00f61f
Farhan Rahman Arnob <farhanarnob@live.com>
2017-03-21 03:06:54 +0600

    Implementing chaining by "forward:"

--- FitnessTracker/src/main/java/com/metafourarnob/controller/MinutesController.java
+++ FitnessTracker/src/main/java/com/metafourarnob/controller/MinutesController.java
@@ -11,6 +11,11 @@ public class MinutesController {
 	@RequestMapping(value="/addMinutes")
 	public String addMinutes(@ModelAttribute("exercise") Exercise exercise){
 		System.out.println("exercise:"+exercise.getMinutes());
+		return "forward:addMoreMinutes.html";
+	}
+	@RequestMapping(value="/addMoreMinutes")
+	public String addMoreMinutes(@ModelAttribute("exercise") Exercise exercise){
+		System.out.println("exercising:"+exercise.getMinutes());
 		return "addMinutes";
 	}
 }
