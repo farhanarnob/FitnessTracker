893fdfbb5e870495d4b2890b8e8ac4d07f359e9e
Farhan Rahman Arnob <farhanarnob@live.com>
2017-03-21 03:10:00 +0600

    Implement chaining by "redirect:"

--- FitnessTracker/src/main/java/com/metafourarnob/controller/MinutesController.java
+++ FitnessTracker/src/main/java/com/metafourarnob/controller/MinutesController.java
@@ -11,7 +11,7 @@ public class MinutesController {
 	@RequestMapping(value="/addMinutes")
 	public String addMinutes(@ModelAttribute("exercise") Exercise exercise){
 		System.out.println("exercise:"+exercise.getMinutes());
-		return "forward:addMoreMinutes.html";
+		return "redirect:addMoreMinutes.html";
 	}
 	@RequestMapping(value="/addMoreMinutes")
 	public String addMoreMinutes(@ModelAttribute("exercise") Exercise exercise){
